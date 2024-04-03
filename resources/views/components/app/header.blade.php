5b6c72bbbc58a24c7d4adae959bcd3d3e7fc8d83
Yannick <yannick.kupferschmidt@googlemail.com>
2024-04-03 15:04:44 +0300

    fix: header bar

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -4,7 +4,7 @@
 
             <!-- Header: Left side -->
             <div class="flex">
-                
+
                 <!-- Hamburger button -->
                 <button
                     class="text-slate-500 hover:text-slate-600 lg:hidden"
@@ -26,16 +26,12 @@
             <div class="flex items-center space-x-3">
 
                 <!-- Search Button with Modal -->
-                <x-modal-search />
 
                 <!-- Notifications button -->
-                <x-dropdown-notifications align="right" />
+                <!-- <x-dropdown-notifications align="right" />-->
 
                 <!-- Info button -->
                 <x-dropdown-help align="right" />
-
-                <!-- Dark mode toggle -->
-                <x-theme-toggle />                
 
                 <!-- Divider -->
                 <hr class="w-px h-6 bg-slate-200 dark:bg-slate-700 border-none" />
--- resources/views/components/dropdown-profile.blade.php
+++ resources/views/components/dropdown-profile.blade.php
@@ -7,9 +7,8 @@
         class="inline-flex justify-center items-center group"
         aria-haspopup="true"
         @click.prevent="open = !open"
-        :aria-expanded="open"                        
+        :aria-expanded="open"
     >
-        <img class="w-8 h-8 rounded-full" src="{{ Auth::user()->profile_photo_url }}" width="32" height="32" alt="{{ Auth::user()->name }}" />
         <div class="flex items-center truncate">
             <span class="truncate ml-2 text-sm font-medium dark:text-slate-300 group-hover:text-slate-800 dark:group-hover:text-slate-200">{{ Auth::user()->name }}</span>
             <svg class="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
@@ -18,7 +17,7 @@
         </div>
     </button>
     <div
-        class="origin-top-right z-10 absolute top-full min-w-44 bg-white dark:bg-slate-800 border border-slate-200 dark:border-slate-700 py-1.5 rounded shadow-lg overflow-hidden mt-1 {{$align === 'right' ? 'right-0' : 'left-0'}}"                
+        class="origin-top-right z-10 absolute top-full min-w-44 bg-white dark:bg-slate-800 border border-slate-200 dark:border-slate-700 py-1.5 rounded shadow-lg overflow-hidden mt-1 {{$align === 'right' ? 'right-0' : 'left-0'}}"
         @click.outside="open = false"
         @keydown.escape.window="open = false"
         x-show="open"
@@ -28,7 +27,7 @@
         x-transition:leave="transition ease-out duration-200"
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
-        x-cloak                    
+        x-cloak
     >
         <div class="pt-0.5 pb-2 px-3 mb-1 border-b border-slate-200 dark:border-slate-700">
             <div class="font-medium text-slate-800 dark:text-slate-100">{{ Auth::user()->name }}</div>
@@ -50,8 +49,8 @@
                     >
                         {{ __('Sign Out') }}
                     </a>
-                </form>                                
+                </form>
             </li>
-        </ul>                
+        </ul>
     </div>
 </div>
